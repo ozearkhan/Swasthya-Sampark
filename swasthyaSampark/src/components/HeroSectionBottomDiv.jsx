bff656abbbe6d83ac41c4f2f402258328e1cf5a9
ozearkhan <ozearkhan1224@gmail.com>
2024-06-13 14:06:46 +0530

    fixed breakpoints width of the heroSVG

--- swasthyaSampark/src/components/HeroSectionBottomDiv.jsx
+++ swasthyaSampark/src/components/HeroSectionBottomDiv.jsx
@@ -3,12 +3,8 @@ import VectorDesign from "./VectorDesign.jsx";
 export function HeroSectionBottomDiv() {
     return (
         <div className="flex justify-center px-4 md:px-6 lg:px-8">
-            {/* Design for larger screens */}
-            <div className="hidden md:flex items-center justify-center w-full max-w-[1280px] lg:max-w-[80%] xl:max-w-[55%] 2xl:max-w-[1360px] custom:max-w-[70%] custom-xl:max-w-[55%] h-auto bg-[#242424] rounded-[16px] p-4">
-                <VectorDesign className="w-full h-full"></VectorDesign>
-            </div>
-            {/* Design for smaller screens */}
-            <div className="flex md:hidden items-center justify-center w-full max-w-[95%] h-auto bg-[#242424] rounded-[16px] p-4">
+            {/* Design for all screens */}
+            <div className="flex items-center justify-center w-full h-auto bg-[#242424] rounded-[16px] p-4 max-w-[95%] sm:max-w-[80%] md:max-w-[95%] lg:max-w-[95%] xl:max-w-[85%] 2xl:max-w-[70%] custom:max-w-[80%] custom-xl:max-w-[55%]">
                 <VectorDesign className="w-full h-full"></VectorDesign>
             </div>
         </div>
