28f5eae44c3182503b38e9311be9884fe51ea3b3
ozearkhan <ozearkhan1224@gmail.com>
2024-08-22 00:28:06 +0530

    Merged and added fixed backend code of the project along with ML model

--- dynamicInput/middleware/inputValidation.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require('express-validator');
-
-exports.validateInput = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-};
