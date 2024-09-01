413138400fd4d11b214ec9291e6a4f82fc76d750
Ozear Khan <ozearkhan1224@gmail.com>
2024-09-01 17:32:50 +0530

    trying img fix

--- swasthyaSampark/src/components/OurSolution.tsx
+++ swasthyaSampark/src/components/OurSolution.tsx
@@ -1,5 +1,5 @@
 import OurSolutionCard from "./OurSolutionCard.tsx";
-import "../../dist/WhoWeHelp/medicalHistory.jpg"
+import "/assets/"
 const OurSolution = ()=>{
     return <div className="flex justify-center items-center flex-col pt-32">
         <h3 className="h3">Our Solution</h3>
@@ -10,12 +10,12 @@ const OurSolution = ()=>{
         </div>
 
         <div className="grid grid-cols-2 gap-4 py-10">
-            <OurSolutionCard imgSrc={"../../dist/WhoWeHelp/checkSymptom.jpg"} heading={"Triage"}
+            <OurSolutionCard imgSrc={"/WhoWeHelp/checkSymptom.jpg"} heading={"Triage"}
                              body={"Integrates into websites, apps, portals, and call centers for symptom checks, self-triage," +
                                  " condition insights, patient education, and care navigation."}
             />
 
-            <OurSolutionCard imgSrc={"../../dist/WhoWeHelp/medicalHistory.jpg"} heading={"Intake"}
+            <OurSolutionCard imgSrc={"/WhoWeHelp/medicalHistory.jpg"} heading={"Intake"}
                              body={"Gather symptoms, risk factors, history, and demographics pre-visit to reduce admin tasks and increase physician-patient time."}
             />
         </div>
